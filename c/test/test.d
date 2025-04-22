85847f376a6f37f7ac4ac959a8d0ce75132fb103
Nickolay Bukreyev <buknik95@ya.ru>
2025-04-23 04:14:30 +0700

    Initial commit

---
+++ c/test/test.d
@@ -0,0 +1,19 @@
+import libstemmer.c;
+
+nothrow pure @system @nogc:
+
+@safe unittest {
+    const a = sb_stemmer_list();
+    assert(a !is null);
+    assert(*a !is null);
+}
+
+unittest {
+    auto st = sb_stemmer_new("ru", null);
+    assert(st !is null);
+    scope(exit) sb_stemmer_delete(st);
+    enum butyavka = "бутявка";
+    const result = sb_stemmer_stem(st, cast(immutable(ubyte)*)butyavka, cast(int)butyavka.length);
+    assert(result !is null);
+    assert(result[0 .. sb_stemmer_length(st)] == "бутявк");
+}
