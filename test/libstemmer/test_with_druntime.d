d89a08cbe335fcea72f3de5efad8b2dd6956eca5
Nickolay Bukreyev <buknik95@ya.ru>
2025-04-25 09:35:27 +0700

    Relicense under BSL-1.0

--- test/libstemmer/test_with_druntime.d
+++ test/libstemmer/test_with_druntime.d
@@ -1,3 +1,7 @@
+// Copyright Nickolay Bukreyev 2025.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file License_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
 module libstemmer.test_with_druntime;
 
 version (D_BetterC) { }
